d1833b59acd1e5798ab7eb49107516a45f24a5f1
Joshua Graber <68428039+joshuagraber@users.noreply.github.com>
2025-03-11 18:09:35 +0000

    chore: config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import generouted from '@generouted/react-router/plugin'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import generouted from "@generouted/react-router/plugin";
 
 export default defineConfig({
-  plugins: [react(), generouted()]
-})
+  plugins: [react(), generouted()],
+  server: {
+    // Allow all hosts in CodeSandbox
+    hmr: {
+      host: "localhost",
+    },
+    host: true,
+    // Add allowed hosts
+    allowedHosts: [
+      "localhost",
+      ".csb.app", // This will allow all CodeSandbox domains
+    ],
+  },
+});
